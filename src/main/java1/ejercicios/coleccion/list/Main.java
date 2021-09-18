c5f203ae3acc110a3811354f79a7848c000d8c70
Deyvi Pilataxi <joelpilataxi@gmail.com>
2021-09-17 20:19:20 -0500

    taller

--- src/main/java1/ejercicios/coleccion/list/Main.java
+++ src/main/java1/ejercicios/coleccion/list/Main.java
@@ -1,10 +1,25 @@
 package ejercicios.coleccion.list;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// ejercicios de list 
+		
+		Integer numero1= 1;
+		Integer numero2= 4;
+		Integer numero3= 8;
+		Integer numero4= 20;
+		
 
+		List<Integer> listaNueva = new ArrayList<Integer>();
+		listaNueva.add(numero1);
+		listaNueva.add(numero2);
+		listaNueva.add(numero3);
+		listaNueva.add(numero4);
 	}
 
 }
