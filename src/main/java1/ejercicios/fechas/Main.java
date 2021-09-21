eb612289b67a058b26a116eea34ebdb82871b9c6
Deyvi Pilataxi <joelpilataxi@gmail.com>
2021-09-20 19:28:58 -0500

    taller fechas

---
+++ src/main/java1/ejercicios/fechas/Main.java
@@ -0,0 +1,19 @@
+package ejercicios.fechas;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Date date= new Date();
+		
+		LocalDate diaHoy = LocalDate.now();
+		System.out.println(diaHoy);
+		
+
+	}
+
+}
